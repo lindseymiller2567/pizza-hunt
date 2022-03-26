46d299bb9bfa85a7267f8c722ae4f911802e6c5a
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-03-26 14:24:30 -0500

    added pizza model and pizza controllers

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,70 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+    // GET /api/pizzas
+    // get all pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // GET /api/pizza/:id
+    // get one pizza
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+            .then(dbPizzaData => {
+                // If no pizza is found, send 404
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id.' });
+                    return;
+                }
+                res.json(dbPizzaData)
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // POST /api/pizzas
+    // create pizza
+    createPizza({ body }, res) {
+        Pizza.create(body)
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    // PUT /api/pizzas/:id
+    // update pizza by id
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true }) // new:true must be included or else mongoose will return the original doc instead of the new doc with the udpated info
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id.' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // DELETE /api/pizzas/:id
+    // delete pizza 
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id.' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = pizzaController;
